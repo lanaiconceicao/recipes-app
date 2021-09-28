3441c619bbbf4153f908aabbb0a346a4d884d2c5
Leonardo Santos <lcds90@gmail.com>
2021-09-28 13:11:37 -0300

    req 43

--- src/components/Share/index.jsx
+++ src/components/Share/index.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import copy from 'clipboard-copy';
 import Button from '../Button';
 import shareIcon from '../../images/shareIcon.svg';
 
-const Share = () => (
-  <Button>
-    <img
-      data-testid="share-btn"
-      src={ shareIcon }
-      alt="share icon"
-    />
-  </Button>
-);
+const Share = () => {
+  const [showCopied, setShowCopied] = useState(false);
+  return (
+    <Button
+      onClick={ () => copy(window.location.href).then(() => {
+        setShowCopied(true);
+      }) }
+    >
+      <img
+        data-testid="share-btn"
+        src={ shareIcon }
+        alt="share icon"
+      />
+      {showCopied && 'Link copiado!'}
+    </Button>
+  );
+};
 
 export default Share;
